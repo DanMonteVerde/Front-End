cb0de417657f6af4a131f20b3566fa40c7209fba
Daniel Barbosa <tvdan2064@gmail.com>
2024-07-28 16:33:37 -0300

    1.0

--- landingpage_01/src/javascript/script.js
+++ landingpage_01/src/javascript/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#mobile_btn").on("click", function() {
         $("#mobile_menu").toggleClass("active");
-        $("#mobile_btn").toggleClass("fa-x");
+        $("#mobile_btn").find("i").toggleClass("fa-x");
     })
 });
   
